2c28c16495512a6e369778807ed984898b7e71fa
NirCohen1 <nircohen133@gmail.com>
2025-01-24 16:54:58 +0200

    commit gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  test: {
-    // enable jest-like global test APIs
-    globals: true,
-    // simulate DOM with happy-dom
-    // (requires installing happy-dom as a peer dependency)
-    environment: 'happy-dom',
-  },
+  plugins: [vue()],
+  base: '/Travel-Itinerary/', // Correct base path for GitHub Pages
 })
