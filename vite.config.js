ac0e5ae764c940b853025d50b96cc8977aecd392
NirCohen1 <81625984+NirCohen1@users.noreply.github.com>
2025-01-24 18:19:43 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  external: ['vue'],
   base: '/Travel-Itinerary/', // Correct base path for GitHub Pages
 })
