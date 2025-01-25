eff85bdd092e2204a3ab77075d45d4c92278feed
NirCohen1 <81625984+NirCohen1@users.noreply.github.com>
2025-01-26 00:29:18 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Travel-Itinerary/', // Ensure this matches your repo name
-  resolve: {
-    alias: {
-      vue: 'vue/dist/vue.esm-bundler.js',
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'
-          }
-        },
-      },
-    },
-  },
+  base: '/Travel-Itinerary/',
 })
