ed4dc78842b9f3e5b3c67970f3ccf23aecec1711
NirCohen1 <nircohen133@gmail.com>
2025-01-25 23:19:23 +0200

    gf pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/Travel-Itinerary/', // Ensure this matches your repo name
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
+    },
+  },
   build: {
     rollupOptions: {
       output: {
