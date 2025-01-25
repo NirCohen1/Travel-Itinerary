a69ee62421e6f2e0bcdb4868f1d2ffd21238122a
NirCohen1 <nircohen133@gmail.com>
2025-01-25 17:45:22 +0200

    create app gh pages

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+// import { createApp } from 'vue';
+import App from './App.vue'
 
-createApp(App).mount('#app'); // Make sure it targets the correct div ID
+Vue.createApp(App).mount('#app')
